dc26f77c1e204ae17a910d91929fa2c7f32b63b8
Ahmy Yulrizka <ahmy@yulrizka.com>
2011-08-12 03:45:46 +0700

    test trap term

--- lib/resque/worker.rb
+++ lib/resque/worker.rb
@@ -114,6 +114,19 @@ module Resque
       puts "<< Calling startup"
       startup
 
+      STDOUT.sync = true
+      puts "Starting up"
+
+      trap('TERM') do
+        puts "Graceful shutdown"
+        exit
+      end
+
+      loop do
+        puts "Pretending to do work"
+        sleep 3
+      end
+
       loop do
         break if shutdown?
 
